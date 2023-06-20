d481a40299721dae253082bb79fbc6134327ab76
Diego Garcia <diegg066.99@gmail.com>
2023-06-20 17:56:10 -0600

    Infierno

---
+++ js/validaciones.js
@@ -0,0 +1,52 @@
+export function valida(input) {
+    const tipoDeInput = input.dataset.tipo;
+    if (validadores[tipoDeInput]) {
+      validadores[tipoDeInput](input);
+    }
+  
+    if (input.validity.valid) {
+      input.parentElement.classList.remove("input-container--invalid");
+      input.parentElement.querySelector(".input-message-error").innerHTML = "";
+    } else {
+      input.parentElement.classList.add("input-container--invalid");
+      input.parentElement.querySelector(".input-message-error").innerHTML =
+        mostrarMensajeDeError(tipoDeInput, input);
+    }
+  }
+  
+  const tipoDeErrores = [
+    "valueMissing",
+    "typeMismatch",
+    "patternMismatch",
+  ];
+  
+  const mensajesDeError = {
+    nombre: {
+      valueMissing: "El campo nombre no puede estar vacío",
+    },
+    email: {
+      valueMissing: "El campo correo no puede estar vacío",
+      typeMismatch: "El correo no es válido",
+    },
+    asunto: {
+      valueMissing: "Este campo no puede estar vacío",
+      patternMismatch: "El asunto debe contener entre 10 a 50 caracteres.",
+    },
+    mensaje: {
+      valueMissing: "Este campo no puede estar vacío",
+      patternMismatch: "El mensaje debe contener entre 10 a 300 caracteres.",
+    },
+  };
+
+function mostrarMensajeDeError(tipoDeInput, input) {
+  let mensaje = "";
+  tipoDeErrores.forEach((error) => {
+    if (input.validity[error]) {
+      console.log(tipoDeInput, error);
+      console.log(input.validity[error]);
+      console.log(mensajesDeError[tipoDeInput][error]);
+      mensaje = mensajesDeError[tipoDeInput][error];
+    }
+  });
+  return mensaje;
+}
